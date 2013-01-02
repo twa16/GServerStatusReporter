2c93dbd21fdde94e438a763501cd744b05f3afd9
MANUEL <MANUEL@DESKTOP1-WIN8>
2013-01-02 03:10:18 -0500

    Added my comments on your code

--- src/me/daneuz23/NServerStatusSR.java
+++ src/me/daneuz23/NServerStatusSR.java
@@ -3,11 +3,10 @@ package me.daneuz23;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mgenterprises.bukkit.Payload;
 import org.mgenterprises.bukkit.ServerStatusPacket;
-import org.mgenterprises.bukkit.event.Listener;
-
 /**
  *
  * @author Home
@@ -26,15 +25,24 @@ public class NServerStatusSR extends JavaPlugin implements Listener {
 
     public void sendUDPPacket(ServerStatusPacket ssp) {
         try {  
+            //GOOD
             int port = ssp.getTargetPort();
+            //This isn't needed since you specified the host below
             InetAddress.getByName("192.168.1.26");
             ssp.setTargetPort(1337);
-            ssp.setPayload(Payload.PLAYER_MAX);
+            //You set the type but never set the data
+            //You are saying "I have NULL max players"  use this.getServer().getMaxPlayers();
+            ssp.setPayload(Payload.PLAYER_MAX); 
+            //unneeded
             ssp.getTargetPort();
+            //unneeded
             ssp.getPayload();
+            //unneeded
             ssp.toString();
+            //GOOD
             byte[] message = ssp.toString().getBytes();
-
+            
+            //GOOD
             InetAddress address = ssp.getTargetHost();
 
             DatagramPacket packet = new DatagramPacket(message, message.length,
