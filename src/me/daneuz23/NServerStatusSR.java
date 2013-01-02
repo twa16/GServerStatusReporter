afd24345b9b66a9c2e6e55f51b333952dfffaeef
Home <Home@192.168.254.4>
2013-01-02 02:06:20 -0600

    Started The targeting

--- src/me/daneuz23/NServerStatusSR.java
+++ src/me/daneuz23/NServerStatusSR.java
@@ -3,9 +3,10 @@ package me.daneuz23;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mgenterprises.bukkit.Payload;
 import org.mgenterprises.bukkit.ServerStatusPacket;
+import org.mgenterprises.bukkit.event.Listener;
 
 /**
  *
@@ -13,22 +14,25 @@ import org.mgenterprises.bukkit.ServerStatusPacket;
  */
 public class NServerStatusSR extends JavaPlugin implements Listener {
 
-    @Override
     public void onEnable() {
         getLogger().info("onEnable has been invoked!");
 
     }
 
-    @Override
     public void onDisable() {
         getLogger().info("onDisable has been invoked!");
 
     }
 
     public void sendUDPPacket(ServerStatusPacket ssp) {
-        try {
+        try {  
             int port = ssp.getTargetPort();
-
+            InetAddress.getByName("192.168.1.26");
+            ssp.setTargetPort(1337);
+            ssp.setPayload(Payload.PLAYER_MAX);
+            ssp.getTargetPort();
+            ssp.getPayload();
+            ssp.toString();
             byte[] message = ssp.toString().getBytes();
 
             InetAddress address = ssp.getTargetHost();
